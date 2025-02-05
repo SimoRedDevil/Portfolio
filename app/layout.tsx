37a5e06e034989d736e196b38043acc0aa9784ef
mel-yous <mohamedyoussfi20@gmail.com>
2025-02-05 15:42:43 +0100

    fix: font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
+        </head>
         <body className='bg-[rgb(19,19,19)]'>
             {children}
         </body>
