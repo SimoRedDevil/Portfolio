a1f2d083cb0fc7d8a3aaa1605d8675dfe5f3dbce
mel-yous <mohamedyoussfi20@gmail.com>
2025-02-08 16:35:06 +0100

    add: title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,11 @@ export default function RootLayout({
     return (
       <html lang="en">
         <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
+          <title>Mohamed El Youssf | Portfolio</title>
         </head>
         <body className='bg-[rgb(19,19,19)]'>
             {children}
