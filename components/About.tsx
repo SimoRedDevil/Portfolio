9c130b6ce8e1b70b9e8bebf1da13af1302bd5283
Mohamed El Youssfi <82889071+SimoRedDevil@users.noreply.github.com>
2025-04-09 15:15:34 +0100

    Update: cv update

--- components/About.tsx
+++ components/About.tsx
@@ -18,7 +18,7 @@ function About() {
           <div className='text-center md:text-left p-3 md:p-6 w-[90%]'>
             <h1 className='text-[30px] md:text-[40px] text-white'>Software Developer</h1>
             <TypeAnimation sequence={['Hey, I’m Mohamed I graduated with a Bachelor’s degree in Economics in 2021. Even though I studied economics, Tech and IT have been my hobby since childhood, and I’ve always loved exploring the world of coding. In 2022, I decided to turn my passion into my career and joined 1337 Coding School, where I learned and worked with technologies like C, C++, React.js, Next.js, Tailwind CSS, Django, Docker, and JavaScript. I enjoy turning people’s ideas into real, working code. I love solving problems, building projects, and learning new things. Whether it’s designing clean interfaces or creating efficient backends, I’m always excited to take on new challenges.']} speed={80} omitDeletionAnimation={true} wrapper='p' className='text-[15px] lg:text-[20px] mt-8 text-white/90 lg:w-[80%] xl:w-[85%]'></TypeAnimation>
-            <a href='/Resume.pdf' target='_blank' className='inline-block transition ease-in-out delay-100 mt-14 border hover:border-white/15 hover:bg-[#00B7C3]/60 border-white/30 rounded-[30px] w-[170px] h-[40px] bg-[#1C1C1C] text-center p-2'>My Resume</a>
+            <a href='/SoftwareDeveloper-CV.pdf' target='_blank' className='inline-block transition ease-in-out delay-100 mt-14 border hover:border-white/15 hover:bg-[#00B7C3]/60 border-white/30 rounded-[30px] w-[170px] h-[40px] bg-[#1C1C1C] text-center p-2'>My Resume</a>
           </div>
         </div>
     </div>
